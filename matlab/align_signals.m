2ad5dff740ff0e9ef9f66dd9d924a855cc11dd38
Matthias <matbloch@student.ethz.ch>
2016-08-02 16:57:58 +0200

    crosscorrelation signal matching

--- matlab/align_signals.m
+++ matlab/align_signals.m
@@ -1,10 +1,9 @@
 close all;
 clc;
 
+%% load the rosbag
+
 filepath = fullfile(fileparts(which('align_signals')), 'data', 'map_0.bag');
-
-
-% load the rosbag
 bag = rosbag(filepath);
 bag.AvailableTopics;
 % bag.MessageList(500:505,:)
@@ -13,79 +12,46 @@ bag.AvailableTopics;
 topic_name = '/vicon/SMOUNT_MB/SMOUNT_MB';
 vicon_select = select(bag,'Topic',topic_name);  % select based on topic
 
-
 % read the data
 % msgs = readMessages(vicon_select);
 % size(msgs)
 
-% msgs{2}
-% msgs{2}.Transform.Translation.Z
 
-% 
-ts = timeseries(vicon_select, 'Transform.Translation.X', 'Transform.Translation.Y', 'Transform.Translation.Z');
+%% extract timeseries and plot
+ts = timeseries(vicon_select, 'Transform.Translation.X', 'Transform.Translation.Y');
 
-figure
-plot(ts, 'LineWidth', 3)
-
-
-% ts.Data
-
-% .Translation, .Rotation (Quaternion)
-
-% plot
-% ts = timeseries(bagMsgs, 'Pose.Pose.Position.X');
-% ts = timeseries(vicon_select)
-
-% vicon_poses = readMessages(vicon_select)
+vicon_x_coords = ts.Data(:,1);
+vicon_x_coords = ts.Data(:,2);
+% figure
+% plot(ts, 'LineWidth', 3)
 
 
+%% load tracker data
 
-% odom = timeseries(bag,'Pose.Pose.Position.X','Pose.Pose.Position.Y');
+filepath = fullfile(fileparts(which('align_signals')), 'data', 'map_0.csv');
+tracker_data = csvread(filename);
+    
+tracker_x_coords = tracker_data(:,1);
+tracker_y_coords = tracker_data(:,2);
 
+%% start alignement
 
-% tform.Transform.Translation
-
-
-
-% msgs = bag.readAll({topic1});
-% 
-% fprintf('Read %i messages\n', length(msgs));
-% 
-% bagselect2 = select(bag, 'Time', ...
-% [bag.StartTime bag.StartTime + 1], 'Topic', topic_name)
-
-
-
-
-break
-
-sz = 30;
-
-% generate random signal: length:
-sg = randn(1,randi(8)+10);   % min length 3
-s1 = [zeros(1,randi(sz)-1) sg zeros(1,randi(sz)-1)];
-
-length = size(sg,2);
-sg = sg + randn(1,length)*0.4;
-
-s2 = [zeros(1,randi(sz)-1) sg zeros(1,randi(sz)-1)];
-
-mx = max(numel(s1),numel(s2));
+mx = max(numel(vicon_x_coords),numel(tracker_x_coords));
 
 ax(1) = subplot(2,1,1);
-stem(s1)
+stem(vicon_x_coords)
 xlim([0 mx+1])
 
 ax(2) = subplot(2,1,2);
-stem(s2,'*')
+stem(tracker_x_coords,'*')
 xlim([0 mx+1])
 
-if numel(s1) > numel(s2)
-    slong = s1;
-    sshort = s2;
+if numel(vicon_x_coords) > numel(tracker_x_coords)
+    slong = vicon_x_coords;
+    sshort = tracker_x_coords;
 else
-    slong = s2;
-    sshort = s1;
+    slong = tracker_x_coords;
+    sshort = vicon_x_coords;
 end
 
 
@@ -99,9 +65,6 @@ stem(lag,acor)
 hold on
 plot(lagDiff,acormax,'*')
 hold off
-
-
-
 
 %{
 Align the signals. Think of the lagging signal as being "longer" than the other, in the sense that you have to "wait longer" to detect it.
