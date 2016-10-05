7da3678c77f1896d968f9332d0247c9d4a0aa92a
Matthias <matbloch@student.ethz.ch>
2016-10-05 21:01:37 +0200

    add camera poses to marker map visualization

--- matlab/plot_marker_map.m
+++ matlab/plot_marker_map.m
@@ -1,3 +1,20 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% @title: Plotting tool for marker tracker measurements
+% @author: Matthias Bloch
+% @version: 1.0
+% 
+% README:
+% Pose logs:
+% .csv file containing pose estimation from vicon and from tracker
+% column format: 
+% [X,Y,Z,RotX,RotY,RotZ,W]_vicon, [-],
+% [X,Y,Z,RotX,RotY,RotZ,W]_tracker
+% X,Y,Z: Position
+% RotX,RotY,RotZ,W: Angle Axis
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 close all;
 clear;
 clc;
@@ -6,63 +23,107 @@ clc;
 [filename,filepath] = uigetfile('*.csv','Select the extracted marker map');
 poseEstimationFile = [filepath,filename];
 
-
 dim_scale = 10; % original: mm
 vector_size = 10;
 
 %% =============================================
-%%      load csv file
+%%      Draw marker map
 
 marker_map = csvread(poseEstimationFile);
-
 sz = size(marker_map);
 nr_markers = sz(1);
 
-
-
-
 %% iterate
 for i = 1:nr_markers
-
-    %
     marker = marker_map(i,:);
     marker_id = marker(1);
+    disp( sprintf( 'Drawing marker %d...', marker_id ) );
     
-    disp( sprintf( 'Drawing marker %d...', marker_id ) )
-    
+    % extract position and rotation matrix
     pos = marker(2:4)/dim_scale;   % cm
     axang = marker(5:8);
-    rotm = axang2rotm(axang)
-
-
-    x_axis = [rotm(1,1), rotm(2,1), rotm(3,1)]*vector_size;
-    y_axis = [rotm(1,2), rotm(2,2), rotm(3,2)]*vector_size;
-    z_axis = [rotm(1,3), rotm(2,3), rotm(3,3)]*vector_size;
-
+    rotm = axang2rotm(axang);
+    
+    % render marker
     hold on
-    q = quiver3(pos(1),pos(2),pos(3),x_axis(1),x_axis(2),x_axis(3))
-    q.Color = 'red';
-    q = quiver3(pos(1),pos(2),pos(3),y_axis(1),y_axis(2),y_axis(3))
-    q.Color = 'green';
-    q = quiver3(pos(1),pos(2),pos(3),z_axis(1),z_axis(2),z_axis(3))
-    q.Color = 'blue';
+    renderPose(pos, rotm, vector_size);
     text(pos(1),pos(2),pos(3),['   ' ...
     num2str(marker_id)],'HorizontalAlignment','left','FontSize',12);
+end
+
+% set aspect ration 1:1
+daspect([1 1 1])
+
+%% =============================================
+%%      Draw reference marker map
+
+% Construct a questdlg with three options
+choice = questdlg('Would you like to load the reference map?', ...
+	'Load marker dataset', ...
+	'Yes','No','No');
+% Handle response
+load_another_ds = 0;
+switch choice
+    case 'Yes'
+        load_another_ds = 1;
+    case 'No'
+        load_another_ds = 0;
+end
+
+% load map log
+if(load_another_ds)
+    [filename,filepath] = uigetfile('*.csv','Select the extracted marker map');
+    poseEstimationFile = [filepath,filename];
+
+    marker_map = csvread(poseEstimationFile);
+    sz = size(marker_map);
+    nr_markers = sz(1);
+
+    %% iterate
+    for i = 1:nr_markers
+        marker = marker_map(i,:);
+        marker_id = marker(1);
+        disp( sprintf( 'Drawing marker %d...', marker_id ) );
+
+        % extract position and rotation matrix
+        pos = marker(2:4)/dim_scale;   % cm
+        axang = marker(5:8);
+        rotm = axang2rotm(axang);
+
+        % render marker
+        hold on
+        renderPose(pos, rotm, vector_size);
+    end
 
 end
 
-% load pose logs
-[filename,filepath] = uigetfile('*.csv','Select pose logs');
-poseEstimationFile = [filepath,filename];
-
 %% =============================================
-%%      load csv file
+%%      Load tracker pose logs
 
-tracker_data = csvread(poseEstimationFile);
+% Construct a questdlg with three options
+choice = questdlg('Would you like to load the synchronized pose logs?', ...
+	'Load pose logs', ...
+	'Yes','No','No');
+% Handle response
+load_camera_logs = 0;
+switch choice
+    case 'Yes'
+        load_camera_logs = 1;
+    case 'No'
+        load_camera_logs = 0;
+end
 
-plot3(tracker_data(:,1)/dim_scale, tracker_data(:,2)/dim_scale, tracker_data(:,3)/dim_scale);
-hold on;
+C={'a', 'b', 'c'};
 
-% axis([-40 20 20 -40 0 40])
-
-zlim([-0.3 40])
+if(load_camera_logs)
+    [filename,filepath] = uigetfile('*.csv','Select pose logs');
+    poseEstimationFile = [filepath,filename];
+    tracker_data = csvread(poseEstimationFile);
+    
+    % draw vicon tracks
+    s1 = scatter3(tracker_data(:,1)/dim_scale, tracker_data(:,2)/dim_scale, tracker_data(:,3)/dim_scale,'filled');
+    % draw tracker position estimation
+    s2 = scatter3(tracker_data(:,9)/dim_scale, tracker_data(:,10)/dim_scale, tracker_data(:,11)/dim_scale,'filled');
+    legend([s1, s2], {'Vicon', 'Marker Tracker'});
+    hold on;
+end
